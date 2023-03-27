27b73cb92f0f434043ff13fc583692f6bf4c0569
Tom Boberek <tomasz.boberek@gmail.com>
2023-03-27 18:07:22 +0100

    Added more dialogs

--- __init__.py
+++ __init__.py
@@ -1,17 +1,42 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util import extract_datetime
+import caldav
 
 
 class CalendarEvents(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def get_credentials(self):
+        self.url = self.settings.get("url", "http://localhost/dav.php")
+        self.username = self.settings.get('username', "test")
+        self.password = self.settings.get('password', "password")
+
+    def initialize(self):
+        self.get_credentials()
+    
+    def shutdown(self):
+        self.speak_dialog('failed.to.execute')
+    
     @intent_file_handler('events.calendar.intent')
     def handle_events_calendar(self, message):
-        date = message.data.get('date')
+        self.initialize()
+        date = extract_datetime(message.data.get('date'))
+        self.speak_dialog('events.calendar')
+        self.principle = self.connect()
 
-        self.speak_dialog('events.calendar', data={
-            'date': date
-        })
+        
+        
+    def connect(self):
+        try:
+            client = caldav.DAVClient(url=self.url, username=self.username, password=self.password)
+            my_principal = client.principal()
+        except (ConnectionError, ConnectionAbortedError, ConnectionAbortedError, TimeoutError):
+            self.speak_dialog('connection.error')
+            self.shutdown()
+        self.speak("I Connected Successfully")
+        return my_principal
+
 
 
 def create_skill():
